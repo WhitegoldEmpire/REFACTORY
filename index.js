de1c8e51788d735f8b114cf3c3c6485eb5ed1fad
WhitegoldEmpire <raquel.rn239@gmail.com>
2019-11-05 16:46:12 +0300

    modified index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+/* app.listen(81, function() {
+//   console.log("listening on 81");
+// });
+// or*/
+
+app.listen(3000, () => {
+  console.log("listening on 3000");
+});
+
+app.get("/", function(req, res) {
+  res.send("Hello World");
+});
