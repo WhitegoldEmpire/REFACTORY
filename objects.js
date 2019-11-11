ce87d39578bf4b7a4bd4b781a9fed047349a7b98
WhitegoldEmpire <raquel.rn239@gmail.com>
2019-11-11 10:05:22 +0300

    objects modified

--- objects.js
+++ objects.js
@@ -1,11 +1,62 @@
-import { directive } from "babel-types";
+/*methods of creating objectExpression
+1. literal method
+2.constructor METHOD
+3.FACTORY METHOD
+ */
 
 
-var lexus = {
-    color: "shiny-black",
-    price: "$34000",
-    mileage: 2000,
-    drive: "fourwheel"
-    model: lexus2240
+/* function phone(model, brand, color,batteryLife,category) {
+    this.model = model;
+    this.brand = brand;
+    this.color = color;
+    this.batteryLife = batteryLife;
+    this.category = category;
+}
 
-};
+let PHONE1 = new phone("A-01","itel", "black", "2000hrs", "katorch");
+let PHONE2 = new phone("GM216", "Iphone", "roseGold", "4000hrs", "smart");
+
+console.log(PHONE1); */
+
+//CREATE MULTIPLE OBJECTS USING CONSTRUCTOR METHOD
+function woman(figure, age, color, occupation, name) {
+    this.figure = figure;
+    this.age = age;
+    this.color = color;
+    this.occupation = occupation;
+    this.name = name;
+}
+
+let WHITEGOLD = new woman("2", "23", "chocolate", "doctor", "racheal");
+let SANDRA = new woman("4", "25", "dark-brown", "banker", "nanteza");
+let CHRISTINE = new woman("3", "28", "brown", "lawyer", "nherza");
+console.log(WHITEGOLD, SANDRA, CHRISTINE);
+//constructor method is advantageous over others, because youcan use only one template and yet create multiple objects with that method.
+
+//using classes
+// class person(){
+// constructor(){
+//     this.name= "jackson"
+//     this.age
+//     this.gender
+// }
+// digest(){
+//     console.log("iam digesting")
+// }
+// }
+
+
+// digest(){
+//         console.log("iam digesting")
+//     }
+
+class Person {
+    constructor(givenName, passedAge, gender) {
+        this.name = givenName;
+        this.age = passedAge;
+        this.gender = gender;
+    }
+
+}
+let shayo = new Person("Ahmed Shayo", 26, "male");
+console.log(shayo);
